32262c78f8e12d54341e06c53af2aa68cee71810
cepthomas <cepthomas@gmail.com>
2024-07-05 14:41:07 -0500

    Names and docs.

--- UserSettings.cs
+++ UserSettings.cs
@@ -27,7 +27,7 @@ namespace ClipPlayer
         public bool AutoClose { get; set; } = true;
 
         [DisplayName("Control Color")]
-        [Description("Pick what you like.")]
+        [Description("The color used for active control surfaces.")]
         [Browsable(true)]
         [JsonConverter(typeof(JsonColorConverter))]
         public Color ControlColor { get; set; } = Color.MediumOrchid;
